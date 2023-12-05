139158c43255c89d404a719be2d996c875c3cb48
mike-ops <mike@Mikes-MacBook-Air.local>
2023-12-05 13:00:14 +0200

    added vscode automation

---
+++ vscode_setup/install-script.sh
@@ -0,0 +1,42 @@
+# microsoft tools
+code --install-extension ms-azuretools.vscode-docker --force
+code --install-extension msazurermtools.azurerm-vscode-tools --force
+code --install-extension ms-vscode.powershell --force
+code --install-extension azps-tools.azps-tools --force
+code --install-extension ms-dotnettools.vscode-dotnet-runtime --force
+
+# remote machine tools
+code --install-extension ms-vscode.remote-explorer --force
+code --install-extension ms-vscode-remote.remote-ssh-edit --force
+code --install-extension ms-vscode-remote.remote-ssh --force
+
+#language tools
+code --install-extension ms-python.python --force
+code --install-extension ms-vscode.vscode-typescript-next --force
+code --install-extension humao.rest-client --force
+code --install-extension yzhang.markdown-all-in-one --force
+code --install-extension dsznajder.es7-react-js-snippets --force
+
+# generally handy tools
+code --install-extension ritwickdey.LiveServer --force
+code --install-extension eamodio.gitlens --force
+code --install-extension usernamehw.errorlens --force
+code --install-extension mtxr.sqltools --force
+
+# formatting and config tools
+code --install-extension esbenp.prettier-vscode --force
+code --install-extension rvest.vs-code-prettier-eslint --force
+code --install-extension dbaeumer.vscode-eslint --force
+code --install-extension EditorConfig.EditorConfig --force
+
+# themes
+code --install-extension EliverLara.andromeda --force
+code --install-extension ahmadawais.shades-of-purple --force 
+
+# disable all extensions so that VSCode doesn't become sluggish
+# only turn on what you use when you use it 
+code --disable-extensions 
+
+# show all of the installed extensions: 
+code --list-extensions
+
---
+++ vscode_setup/readme.md
@@ -0,0 +1,8 @@
+# Automate installing extensions on VSCode :wrench:
+
+### Run the script
+
+- run: `sudo zsh ./install-script.sh` in a terminal window from your chosen root directory.
+- the `--force` flag is used to avoid all prompts and will install the latest verion of an extension.
+- `--disable-extensions` will disable all extensions so that VSCode doesn't become sluggish.
+- `--list-extensions` will list all installed extensions to the terminal.
